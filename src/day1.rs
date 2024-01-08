7ac0b8d741e853333c7af3ed1edcc5e60019ba7a
Bruno Mello <bruno.barros.mello@gmail.com>
2024-01-08 20:46:45 -0300

    day1 rework

--- src/day1.rs
+++ src/day1.rs
@@ -2,33 +2,13 @@ pub fn run() {
     run_part_2();
 }
 
-const NUMERIC_WORDS: [&str; 9] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-];
+type WordData = (String, usize);
+type DigitData = (char, usize);
 
 #[allow(dead_code)]
 fn run_part_1() {
     let contents = std::fs::read_to_string("input/day1.txt").unwrap();
 
-    let sum = get_sum(contents);
-
-    println!("sum is {}", sum);
-}
-
-fn run_part_2() {
-    let contents = std::fs::read_to_string("input/day1.txt").unwrap();
-    let mut cleaned_lines = vec![];
-
-    for line in contents.lines() {
-        let cleaned_line = get_cleaned_line(line.to_string());
-        cleaned_lines.push(cleaned_line);
-    }
-
-    let sum = get_sum(cleaned_lines.join("\n"));
-    println!("sum is {}", sum);
-}
-
-fn get_sum(contents: String) -> usize {
     let mut sum = 0;
 
     for line in contents.lines() {
@@ -47,150 +27,215 @@ fn get_sum(contents: String) -> usize {
         sum += combined;
     }
 
-    return sum as usize;
+    println!("sum is {}", sum);
+}
+
+fn run_part_2() {
+    let contents = std::fs::read_to_string("input/day1.txt").unwrap();
+
+    let mut sum = 0;
+
+    for line in contents.lines() {
+        let combined = get_combined_of_line_considering_words(line.to_string());
+        let combined = combined.parse::<i32>().unwrap();
+        sum += combined;
+    }
+
+    println!("sum is {}", sum);
+}
+
+fn get_combined_of_line_considering_words(line: String) -> String {
+    let first_word = get_first_word_with_index(line.to_string());
+    let last_word = get_last_word_with_index(line.to_string());
+    let first_digit = get_first_digit_with_index(line.to_string());
+    let last_digit = get_last_digit_with_index(line.to_string());
+
+    let first_digit_to_append = if first_word.1 < first_digit.1 {
+        word_to_digit(first_word.0)
+    } else {
+        first_digit.0
+    };
+
+    let last_digit_to_append = if last_word.1 > last_digit.1 {
+        word_to_digit(last_word.0)
+    } else {
+        last_digit.0
+    };
+
+    let combined = format!("{}{}", first_digit_to_append, last_digit_to_append);
+
+    return combined;
+}
+
+fn word_to_digit(word: String) -> char {
+    match word.as_str() {
+        "one" => '1',
+        "two" => '2',
+        "three" => '3',
+        "four" => '4',
+        "five" => '5',
+        "six" => '6',
+        "seven" => '7',
+        "eight" => '8',
+        "nine" => '9',
+        _ => unreachable!("should never have gotten here when converting word to digit"),
+    }
 }
 
 fn is_numeric(ch: char) -> bool {
     return ch >= '0' && ch <= '9';
 }
 
-fn replace_numeric_word(line: String) -> String {
-    for word in NUMERIC_WORDS {
-        if line.contains(word) {
-            let digit = get_numeric_word_digit(word);
-            return line.replace(word, &digit.to_string());
+fn get_first_word_with_index(line: String) -> WordData {
+    let words = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut first_word = String::new();
+    let mut first_index = usize::MAX;
+
+    for word in words.iter() {
+        if let Some(index) = line.find(*word) {
+            if index < first_index {
+                first_word = word.to_string();
+                first_index = index;
+            }
         }
     }
 
-    return line;
+    return (first_word, first_index);
 }
 
-fn get_cleaned_line(line: String) -> String {
-    let mut consuming = "".to_string();
+fn get_last_word_with_index(line: String) -> WordData {
+    let words = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut last_word = String::new();
+    let mut last_index = usize::MIN;
 
-    for ch in line.chars() {
-        consuming.push(ch);
-        if has_numeric_word(&consuming) {
-            consuming = replace_numeric_word(consuming);
+    for word in words.iter() {
+        if let Some(index) = line.rfind(*word) {
+            if index > last_index {
+                last_word = word.to_string();
+                last_index = index;
+            }
         }
     }
 
-    return consuming;
+    return (last_word, last_index);
 }
 
-fn get_numeric_word_digit(word: &str) -> usize {
-    match word {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        _ => unreachable!(),
-    }
-}
+fn get_first_digit_with_index(line: String) -> DigitData {
+    let digits = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-fn has_numeric_word(line: &String) -> bool {
-    for word in NUMERIC_WORDS {
-        if line.contains(word) {
-            return true;
+    let mut first_digit = '0';
+    let mut first_index = usize::MAX;
+
+    for digit in digits.iter() {
+        if let Some(index) = line.find(std::char::from_digit(*digit as u32, 10).unwrap()) {
+            if index < first_index {
+                first_digit = std::char::from_digit(*digit as u32, 10).unwrap();
+                first_index = index;
+            }
         }
     }
 
-    return false;
+    return (first_digit, first_index);
+}
+
+fn get_last_digit_with_index(line: String) -> DigitData {
+    let digits = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let mut last_digit = '0';
+    let mut last_index = usize::MIN;
+
+    for digit in digits.iter() {
+        if let Some(index) = line.rfind(std::char::from_digit(*digit as u32, 10).unwrap()) {
+            if index > last_index {
+                last_digit = std::char::from_digit(*digit as u32, 10).unwrap();
+                last_index = index;
+            }
+        }
+    }
+
+    return (last_digit, last_index);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day1::get_sum;
-
-    use super::get_cleaned_line;
+    use crate::day1::{
+        get_combined_of_line_considering_words, get_first_word_with_index, get_last_word_with_index,
+    };
 
     #[test]
-    fn test_get_cleaned_line() {
-        assert_eq!(
-            get_cleaned_line("4bvnccbdh4onefztdrpq62vvbnvpxxvgrngnfjgfk".to_string()),
-            "4bvnccbdh41fztdrpq62vvbnvpxxvgrngnfjgfk"
-        );
-        assert_eq!(
-            get_cleaned_line("2ninebvgdcfxtktqjxjqvxfgjdqfhv5threegqtsfhtfxg".to_string()),
-            "29bvgdcfxtktqjxjqvxfgjdqfhv53gqtsfhtfxg"
-        );
-        assert_eq!(
-            get_cleaned_line("xtwo7threemxbtpsvjkgrfivethree2".to_string()),
-            "x273mxbtpsvjkgr532"
-        );
-        assert_eq!(
-            get_cleaned_line("cqoneighteightjnrfkplvninefivemck18mnhszhkv4".to_string()),
-            "cq1ight8jnrfkplv95mck18mnhszhkv4"
-        );
-        assert_eq!(
-            get_cleaned_line("tbvdcsjsvmxtshv3fourseven4kmxvvfour9".to_string()),
-            "tbvdcsjsvmxtshv3474kmxvv49"
-        );
-        assert_eq!(
-            get_cleaned_line("bxcsix19six8dnqsbx".to_string()),
-            "bxc61968dnqsbx"
-        );
+    fn test_get_first_word_with_index() {
+        let tests = vec![
+            ("12onetwothreefour", ("one", 2)),
+            ("aaaaatwoone", ("two", 5)),
+        ];
+
+        for test in tests {
+            let input = test.0.to_string();
+            let expected_result = (test.1 .0.to_string(), test.1 .1);
+            assert_eq!(get_first_word_with_index(input), expected_result);
+        }
     }
 
     #[test]
-    fn test_get_sum() {
+    fn test_get_last_word_with_index() {
         let tests = vec![
-            ("n7", 77),
-            ("1three2", 12),
-            ("cqoneighteightjnrfkplvninefivemck18mnhszhkv4", 14),
-            ("tbvdcsjsvmxtshv3fourseven4kmxvvfour9", 39),
-            ("bxcsix19six8dnqsbx", 68),
-            ("cqoneighteightjnrfkplvninefivemck18mnhszhkv4\ntbvdcsjsvmxtshv3fourseven4kmxvvfour9\nbxcsix19six8dnqsbx", 14 + 39 + 68),
-            ("2ninebvgdcfxtktqjxjqvxfgjdqfhv5threegqtsfhtfxg", 23),
-            ("6threev", 63),
-            ("69sixnine", 69),
-            ("1btphrrvxdeightonekdhv8", 18),
-            ("ninerlsbznvfn9", 99),
-            ("sixthree6lxcrsevenseven69twonegs", 62),
-            ("xrbtzbklqsl11", 11),
-            ("5p", 55),
-            ("66bnfj", 66),
-            ("6bnfj", 66),
-            ("bnfj6", 66),
-            ("bnfjsix", 66),
-            ("sixbnfjix", 66),
-            ("6bnfj", 66),
-            ("bn655556fj", 66),
-            ("bn65fivefj", 65),
-            ("5lclone", 51),
-            ("six89bdlssd", 69),
-            ("tbvdcsjsvmxtshv3fourseven4kmxvvfour9", 39),
-            ("f1lhrbsix", 16),
-            ("mbbkv7ffpk", 77),
-            ("9eightseventhree", 93),
-            ("9jpvccsvhqpnhsl8", 98),
-            ("nvcnninefour9", 99),
-            ("v237ppqbhb", 27),
-            ("78six", 76),
-            ("9eightseventhree", 93),
-            ("9jpvccsvhqpnhsl8", 98),
-            ("nvcnninefour9", 99),
-            ("v237ppqbhb", 27),
-            ("78six", 76),
-            ("four165oneightxcm",41),
-            ("9pjcsfbrghnineqzth4smx", 94),
-            ("46fpfptrq1mbqmbnktqeight", 48),
-            ("stbxvlcqz5krd1threethreeonefour", 54),
-            ("48six5seven", 47),
-            ("6bmltlrvrgpcfhjhmfiveqzfxptjtwo4zvsqqxgbrdlzsfmtzdd", 64),
-            (
-            "9eightseventhree\n9jpvccsvhqpnhsl8\nnvcnninefour9\nv237ppqbhb\n78six\nfour165oneightxcm\n9pjcsfbrghnineqzth4smx\n46fpfptrq1mbqmbnktqeight\nstbxvlcqz5krd1threethreeonefour\n48six5seven\n6bmltlrvrgpcfhjhmfiveqzfxptjtwo4zvsqqxgbrdlzsfmtzdd",
-            93 + 98 + 99 + 27 + 76 + 41 + 94 + 48 + 54 + 47 + 64
-            )
-
+            ("12onetwothreefour", ("four", 13)),
+            ("aaaaatwoone", ("one", 8)),
+            ("aaaaatwone", ("one", 7)),
         ];
+
         for test in tests {
-            assert_eq!(get_sum(get_cleaned_line(test.0.to_string())), test.1);
+            let input = test.0.to_string();
+            let expected_result = (test.1 .0.to_string(), test.1 .1);
+            assert_eq!(get_last_word_with_index(input), expected_result);
+        }
+    }
+
+    #[test]
+    fn test_get_first_digit_with_index() {
+        let tests = vec![
+            ("012onetwo34", ('1', 1)),
+            ("12onetwo34", ('1', 0)),
+            ("aaaaaa21onetwo34", ('2', 6)),
+            ("22221onetwo34", ('2', 0)),
+        ];
+
+        for test in tests {
+            let input = test.0.to_string();
+            let expected_result = (test.1 .0, test.1 .1);
+            assert_eq!(super::get_first_digit_with_index(input), expected_result);
+        }
+    }
+
+    #[test]
+    fn test_get_last_digit_with_index() {
+        let tests = vec![
+            ("12onetwo34", ('4', 9)),
+            ("12onetwo340", ('4', 9)),
+            ("aaaaaa21onetwo34", ('4', 15)),
+            ("22221onetwo34", ('4', 12)),
+        ];
+
+        for test in tests {
+            let input = test.0.to_string();
+            let expected_result = (test.1 .0, test.1 .1);
+            assert_eq!(super::get_last_digit_with_index(input), expected_result);
+        }
+    }
+
+    #[test]
+    fn test_get_combined_considering_words() {
+        let tests = vec![("sixthree6lxcrsevenseven69twonegs", "61")];
+
+        for test in tests {
+            assert_eq!(
+                get_combined_of_line_considering_words(test.0.to_string()),
+                test.1.to_string()
+            );
         }
     }
 }
